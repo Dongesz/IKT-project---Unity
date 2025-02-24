15396ee92bec8b4e61b69650ebbfab7c88aa946c
Dongesz <majormate12.com>
2025-02-24 18:33:33 +0100

    Implemented level transition system using SceneAsset list for seamless progression

---
+++ Assets/Scripts/Core/LevelManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[System.Serializable]
+public class SceneReference
+{
+    public SceneAsset scene; // Unity-ben választható Scene
+}
+
+public class LevelManager : MonoBehaviour
+{
+    [SerializeField] private List<SceneReference> sceneOrder; // SceneAsset alapú lista
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            LoadNextLevel();
+        }
+    }
+
+    void LoadNextLevel()
+    {
+        string currentScene = SceneManager.GetActiveScene().name;
+        int currentIndex = sceneOrder.FindIndex(s => s.scene.name == currentScene); // Keresés a listában
+
+        if (currentIndex != -1 && currentIndex + 1 < sceneOrder.Count)
+        {
+            string nextSceneName = sceneOrder[currentIndex + 1].scene.name;
+            SceneManager.LoadScene(nextSceneName);
+        }
+        else
+        {
+            Debug.Log("Nincs több elérhető pálya! Játék vége.");
+        }
+    }
+}
